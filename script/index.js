ffde0c6690111f32e9d0742bfac9a599cec477af
Nazim Shala <shalanazim@gmail.com>
2019-09-17 18:08:50 +0200

    add new input & delete inout

--- script/index.js
+++ script/index.js
@@ -1,36 +1,59 @@
 window.addEventListener("DOMContentLoaded", ()=> {
-  
-  let toDoList = document.querySelector("#to-do-list");
-  let toDoItems = document.querySelector("#to-do-list").children;
-  let toDoListArr = [];
-  let newItem = "";
-  console.log(toDoItems)
-  for (let i = 0; i < toDoList.length; i++) {
-    toDoList[i].addEventListener("click", done, false);
-    toDoListArr.push(toDoList[i].firstChild.innerText);
-  }
-  for (let i = 0; i < toDoItems.length; i++) {
-    toDoItems[i].addEventListener("click", done, false);
-  }
-  
-  const addItem = document.querySelector("#submit");
-  addItem.addEventListener("click", addNewItem, false);
 
+  const deleteItem = document.querySelector("span");
+  deleteItem.style.display = "none";  
+
+  let toDoList = document.querySelector("#to-do-list");
+  let newItem = "";
+  let toDoListArr = [];
+  let randomeText = [ "Wow nothing to do! Awesome...", 
+  "All done! Way to go", 
+  "All empty here, wanna add something to do?",
+  "Ready to chill? Cause there is nothing on this list" ];
+
+  const randomeTextInput = document.querySelector(".randomText").firstChild;
+  const addItem = document.querySelector("#submit");
+  addItem.addEventListener("submit", addNewItem, false);
+  
+  
+  rendom();
+  
+  // addeds a rendome text if List is emplty
+  function rendom() {
+    if (toDoListArr.length === 0) {
+      randomeTextInput.style.display = "flex"
+      let randomeTextIndex = Math.floor(Math.random() * randomeText.length);
+      randomeTextInput.innerText = randomeText[Number(randomeTextIndex)];
+    }
+  }
+  
   // Takes the Input Value and calls the showAddedItem().
-  function addNewItem() {
+  function addNewItem(e) {
+    e.preventDefault();
+
     let input = document.querySelector("[name=todo]");
     newItem = input.value;
     input.value = "";
     showAddedItem();
   }
+  
   // showAddedItem() adds the Input Value to the To Do List.
   function showAddedItem() {
+    randomeTextInput.style.display = "none"
+    
     if (newItem !== "") {
-      toDoListArr.push(newItem)
       let newItemLi = document.createElement("li");
-      toDoList.appendChild(newItemLi).appendChild(document.createElement("p")).innerText = newItem;
-  console.log(toDoItems)
-
+      let closeButton = deleteItem.cloneNode(true);
+      closeButton.style.display = 'inline-block';
+      
+      newItemLi.appendChild(document.createElement("p")).innerText = newItem;
+      newItemLi.appendChild(closeButton)
+      
+      newItemLi.addEventListener("click", done, false);
+      closeButton.addEventListener("click", deleteSelectedItem, false);
+      
+      toDoList.appendChild(newItemLi)
+      toDoListArr.push(newItemLi);
     }
   }
   
@@ -39,7 +62,13 @@ window.addEventListener("DOMContentLoaded", ()=> {
     if (event.target.nodeName === "P") {
       event.target.classList.add("done");
     }
-    
+  }
+  
+  // delete Item of the List
+  function deleteSelectedItem(e) {
+    toDoListArr.pop(e.target.parentNode);
+    e.target.parentNode.style.display = "none";
+    rendom();
   }
   
 });
